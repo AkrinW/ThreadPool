1cbf855cc085cae41f947ef2036fc6e0c92407ad
akarinDesktop <791972041@qq.com>
2024-06-12 00:23:52 +0800

    threadpool.h

--- include/ThreadPool.h
+++ include/ThreadPool.h
@@ -6,13 +6,21 @@
 #include <vector>
 #include <queue>
 #include <functional>
-
+#include <mutex>
+#include <condition_variable>
+#include <future>
 class ThreadPool {
 private:
-    std::vector<std::thread> pool;
+    std::vector<std::thread> workers;//工作的线程
+    std::queue<std::function<void()>> tasks;//工作队列
+    std::mutex queueMutex;//互斥锁
+    bool stop;
+    std::condition_variable condition;
 public:
     ThreadPool(int num);
     ~ThreadPool();
+    template<class F, class... Args>
+    auto enqueue(F&& f, Args&&... args) -> std::future<typename std::result_of<F(Args...)>::type>;
 };
 
 
